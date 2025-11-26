ac4ca5f687e989479c31aafcedac156ede61cae0
Aryan <aryanpriyansh13@gmail.com>
2025-11-26 16:47:24 +0530

    Add python-dotenv for Render

--- Backend/main.py
+++ Backend/main.py
@@ -330,7 +330,7 @@ app = FastAPI(title="College Chatbot API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
